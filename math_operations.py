aba1408d6201b2902337ca19006d9bfc7cbb9a82
Srimahalakshmi <srimahalakshmi62004@gmail.com>
2024-07-06 02:01:53 +0530

    firstcommit

---
+++ math_operations.py
@@ -0,0 +1,21 @@
+
+def addition(a,b):
+    print("Result of Adding  numbers",a,",",b,"is")
+    return (a+b)
+
+
+def subtraction(c,d):
+    print("Result of subtracting  numbers",c,",",d,"is")
+    return (c-d)
+
+def multiplication(e,f):
+    print("Result of multiplication of numbers",e,",",f,"is")
+    return (e*f)
+
+def division(g,h):
+    print("Result of dividing numbers",g,",",h,"is")
+    return (g/h)
+
+def power(i,j):
+    print("Result of" ,i,"power",j,"is")
+    return(i**j)
---
+++ math_operations_script.py
@@ -0,0 +1,17 @@
+import math_operations as m
+
+
+a=m.addition(10,20)  #adding two numbers
+print(a)
+
+ 
+b=m.subtraction(5,4)  #subtracting two numbers
+print(b)
+ 
+c=m.division(10,5)   #Division of two numbers
+print(c)
+ 
+d=m.power(2,3)  # power of two numbers
+print(d)
+
+
